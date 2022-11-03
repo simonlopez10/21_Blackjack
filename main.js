9490b4ae53bb281fabcad177808a0fd40e3189ca
Simon Lopez Calderon <simon.lopezcalde@gmail.com>
2022-11-02 20:15:02 -0500

    instructions and deck image

---
+++ main.js
@@ -0,0 +1,6 @@
+
+// Crear funcion que reciba como parametro el juagador o el dealer para entregar carta aleatoria
+
+// delaer decide si recibe carta o no... revisar un valor maximo para que el dealear pida carta, 
+// establecer el limite de riesgo del delaer
+ 
