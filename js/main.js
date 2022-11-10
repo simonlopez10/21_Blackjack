ea9cb69cf41af8525c2dff2274e47070b8c82460
Simon Lopez Calderon <simon.lopezcalde@gmail.com>
2022-11-10 17:22:19 -0500

    changes and new functions

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@ var hiddenCard;
 // VG - PLAYER
 var playerPoints;
 var playerAce;
-var hitCard;
+var canHitCard = true;
 
 
 function initialState() {
@@ -18,8 +18,9 @@ function initialState() {
     document.getElementById('cards-and-results').style.display = 'none'
 }
 
+
 function createDeck() {
-    // Crea el mazo de cartas
+    // Crea el mazo de cartas iterando valores por cada pinta
     let values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
     let types = ["C", "D", "H", "S"]
     deck = [];
@@ -33,6 +34,7 @@ function createDeck() {
     //console.log(deck)
 }
 
+
 function shuffleDeck(arr) {
     // mezcla el mazo de manera aleatoria usando algoritmo fisher-yates
     let i = arr.length;
@@ -44,17 +46,12 @@ function shuffleDeck(arr) {
 }
 
 
-
-function startGame() {
-
-    hiddenCard = deck.pop();
-    dealerPoints += getCardValue(hiddenCard);
-    dealerAce += checkAce(hiddenCard);
-    console.log(hiddenCard);
-    console.log(dealerPoints);
-
-
+function showGame()  {
+    // Muestra las condiciones iniciales del juego al presionar el boton start game
+    startButton = document.getElementById('start-button');
+    startButton.addEventListener('click', startGame)
 }
+
 
 function getCardValue(card) {
 
@@ -73,15 +70,119 @@ function getCardValue(card) {
     return parseInt(cardValue)
 }
 
+
 function checkAce(card) {
+    // revisa si la carta recibida es un As. devuelve 1 si es verdadero y 0 si no lo es
     if (card[0] == "A") {
         return 1;
     } 
     return 0;
 }
 
+
+function startGame() {
+
+    // funcion de juego principal
+
+    // Muestra los elementos ocutlos de la condición inicial
+    document.getElementById('deck-and-buttons-container').style.display = 'block';
+    document.getElementById('cards-and-results').style.display = 'block';
+    document.getElementById('start-button').style.display = 'none';
+    document.getElementById('top-instruction').style.paddingTop = '9.5%'
+
+    // Saca una carte del mazo para darle valor a la carta volteada del dealer
+    hiddenCard = deck.pop();
+    dealerPoints += getCardValue(hiddenCard); // Devuelve el valor de dicha carta
+    dealerAce += checkAce(hiddenCard); // Valida si la carta volteada es un As
+    // console.log(hiddenCard);
+    // console.log(dealerPoints);
+
+    // Entrega cartas al dealer mienstras que la suma sea inferior a 17 (contando la carta oculta)
+    while (dealerPoints < 17) {
+        let cardImg = document.createElement("img");
+        let card = deck.pop();
+        cardImg.src = "/cards/" + card + ".png";
+        dealerPoints += getCardValue(card);
+        dealerAce += checkAce(card);
+        document.getElementById("dealer-cards").append(cardImg);
+    }
+    console.log(dealerPoints)
+
+    for (let i = 0; i < 2; i++) {
+        let cardImg = document.createElement("img");
+        let card = deck.pop();
+        cardImg.src = "/cards/" + card + ".png";
+        playerPoints += getCardValue(card);
+        playerAce += checkAce(card);
+        document.getElementById("player-cards").append(cardImg);
+    }
+
+    document.getElementById('hit-button').addEventListener('click', hitForCard)
+    document.getElementById('hold-button').addEventListener('click', holdPoints)
+}
+
+
+function hitForCard() {
+
+    if (!canHitCard) {
+        return;
+    }
+
+    let cardImg = document.createElement("img");
+    let card = deck.pop();
+    cardImg.src = "/cards/" + card + ".png";
+    playerPoints += getCardValue(card);
+    playerAce += checkAce(card);
+    document.getElementById("player-cards").append(cardImg);
+
+    if (transformAce(playerPoints, playerAce) > 21) {
+        canHitCard = false;
+    }
+
+    showLenghtDeck();
+}
+
+
+function holdPoints() {
+    dealerPoints = transformAce(dealerPoints, dealerAce);
+    playerPoints = transformAce(playerPoints, playerAce);
+
+    canHitCard = false;
+    document.getElementById('hidden-card').src = "/cards/" + hiddenCard + ".png";
+
+    // Validar si jugador o dealaer se pasan de 21 puntos
+    let resultMessage = "";
+    if (playerPoints > 21) {
+        resultMessage = "YOU LOSE!";
+    } else if (dealerPoints > 21) {
+        resultMessage = "YOU WIN!";
+    } else if (playerPoints == dealerPoints) {  // Comparar el resultado de las sumas de puntas si ninguno se ha pasado
+        resultMessage = 'TIE!'
+    }  else if (playerPoints > dealerPoints) {
+        resultMessage = "YOU WIN!";
+    } else if (playerPoints < dealerPoints) {
+        resultMessage = "YOU LOSE!"
+    }
+
+    document.getElementById('result-message').innerText = resultMessage
+
+}
+
+
+function transformAce(playerPoints, playerAce) {
+    // Si la suma es mayor a 21, se reestablece el valor de la As (ya no vale 11 sino que vale 1) y
+    // se le resta uno al contador de As que tiene el jugador (playerAce)
+    while (playerPoints > 21 && playerAce > 0) {
+        playerPoints -= 10;
+        playerAce -= 1;
+    }
+    return playerPoints;
+}
+
+
 window.onload = function () {
+    // Al cargar la pagina, ejecuta las siguientes funciones
     initialState();
     createDeck();
-    startGame();
+    showGame();
 }
